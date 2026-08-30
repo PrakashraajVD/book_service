d30e6c73027b524cb35f8c1bb97311588198bd5d
PrakashraajVD <prakashraajvd2004@gmail.com>
2026-08-30 15:57:52 +0530

    feat: initialize server

---
+++ crates/book_service/src/bin/app.rs
@@ -0,0 +1,29 @@
+use book_service::routes;
+use tracing::info;
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        "info,tower_http=warn,axum=warn,toasty=warn,tokio-postgres=warn".into()
+    });
+
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(
+            tracing_subscriber::fmt::layer()
+                .json()
+                .with_target(false)
+                .with_current_span(false),
+        )
+        .init();
+
+    let addr = String::from("0.0.0.0:3000");
+    info!(addr=%addr, "Starting server");
+
+    let listener = tokio::net::TcpListener::bind(&addr).await?;
+    let app = routes::init();
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
